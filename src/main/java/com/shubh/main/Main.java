81d0de8c4be50202c43f0c48d53903aeae64c033
Shubham Kumar <Shubham.Kumar@Geminisolutions.com>
2022-10-07 16:02:24 +0530

    sample-repo

---
+++ src/main/java/com/shubh/main/Main.java
@@ -0,0 +1,20 @@
+package com.shubh.main;
+
+public class Main {
+
+    public int computeAddition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int computeDifference(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int computeMultiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int computeDivision(int num1, int num2) {
+        return num1 / num2;
+    }
+}
